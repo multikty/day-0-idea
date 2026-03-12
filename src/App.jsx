0f22ecb0b9df16828524063be2a00829a7fd8e10
multikty <multikty@gmail.com>
2026-03-12 15:25:34 +0900

    Upgrade UI to premium light theme with glassmorphism

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 function App() {
   return (
     <div className="container">
-      <h1 className="idea-text">내 첫 번째 아이디어</h1>
+      <h1 className="idea-text">오늘도 힘내세요!</h1>
       <p className="sub-text">환영합니다. 당신의 상상력을 펼쳐보세요.</p>
     </div>
   )
