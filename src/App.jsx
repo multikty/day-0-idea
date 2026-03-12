72d4b6c780f7391f42820ffa1e4b72aaf4999d1e
multikty <multikty@gmail.com>
2026-03-12 13:54:07 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="idea-text">내 첫 번째 아이디어</h1>
+      <p className="sub-text">Inspired by Vision</p>
+    </div>
+  )
+}
+
+export default App
