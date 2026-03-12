41399b76bbc2e4920c9eb50646cb4a1546fbc03f
multikty <multikty@gmail.com>
2026-03-12 14:24:09 +0900

    Update welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1 className="idea-text">내 첫 번째 아이디어</h1>
-      <p className="sub-text">Inspired by Vision</p>
+      <p className="sub-text">환영합니다. 당신의 상상력을 펼쳐보세요.</p>
     </div>
   )
 }
